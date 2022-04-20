1789fa1378e30bcc3494429df13d6c5f355b717e
Abhiroopmokshagna <abhiroopmokshagnabheemineni@gmail.com>
2022-04-20 10:37:12 +0530

    Front end modules and webpack

--- ui/src/App.jsx
+++ ui/src/App.jsx
@@ -1,3 +1,5 @@
+import graphQLFetch from "./graphQLFetch";
+
 const initialIssues = [
   {
     id: 1,
@@ -23,13 +25,6 @@ class IssueFilter extends React.Component {
   render() {
     return <div>This is a placeholder for the issue filter.</div>;
   }
-}
-
-const dateRegex = new RegExp("^\\d\\d\\d\\d-\\d\\d-\\d\\d");
-
-function jsonDateReviver(key, value) {
-  if (dateRegex.test(value)) return new Date(value);
-  return value;
 }
 
 function IssueRow(props) {
@@ -94,31 +89,6 @@ class IssueAdd extends React.Component {
         <button>Add</button>
       </form>
     );
-  }
-}
-
-async function graphQLFetch(query, variables = {}) {
-  try {
-    const response = await fetch(window.ENV.UI_API_ENDPOINT, {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ query, variables }),
-    });
-    const body = await response.text();
-    const result = JSON.parse(body, jsonDateReviver);
-
-    if (result.errors) {
-      const error = result.errors[0];
-      if (error.extensions.code == "BAD_USER_INPUT") {
-        const details = error.extensions.exception.errors.join("\n ");
-        alert(`${error.message}:\n ${details}`);
-      } else {
-        alert(`${error.extensions.code}: ${error.message}`);
-      }
-    }
-    return result.data;
-  } catch (e) {
-    alert(`Error in sending data to server: ${e.message}`);
   }
 }
 
