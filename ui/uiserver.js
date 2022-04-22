892af44e15ec57678dde162581aacffa1f707431
Abhiroopmokshagna <abhiroopmokshagnabheemineni@gmail.com>
2022-04-22 12:55:49 +0530

    transform, bundle, hot module replacement

--- ui/uiserver.js
+++ ui/uiserver.js
@@ -9,6 +9,25 @@ const env = { UI_API_ENDPOINT };
 
 const app = express();
 
+const enableHMR = (process.env.ENABLE_HMR || "true") === "true";
+
+if (enableHMR && process.env.NODE_ENV !== "production") {
+  console.log("Adding dev middleware, enabling HMR");
+
+  const webpack = require("webpack");
+  const devMiddleware = require("webpack-dev-middleware");
+  const hotMiddleware = require("webpack-hot-middleware");
+
+  const config = require("./webpack.config.js");
+  config.entry.app.push("webpack-hot-middleware/client");
+  config.plugins = config.plugins || [];
+  config.plugins.push(new webpack.HotModuleReplacementPlugin());
+
+  const compiler = webpack(config);
+  app.use(devMiddleware(compiler));
+  app.use(hotMiddleware(compiler));
+}
+
 const apiProxyTarget = process.env.API_PROXY_TARGET;
 if (apiProxyTarget) {
   app.use("/graphql", proxy({ target: apiProxyTarget }));
