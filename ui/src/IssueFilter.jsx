80589db5eac0c40e9fd23d86a9737c4586523226
Abhiroopmokshagna <abhiroopmokshagnabheemineni@gmail.com>
2022-04-22 18:15:52 +0530

    Programmatic navigation

--- ui/src/IssueFilter.jsx
+++ ui/src/IssueFilter.jsx
@@ -1,10 +1,22 @@
 import React from "react";
-import { Link } from "react-router-dom";
-export default class IssueFilter extends React.Component {
+import { withRouter } from "react-router-dom";
+class IssueFilter extends React.Component {
+  constructor() {
+    super();
+    this.onChangeStatus = this.onChangeStatus.bind(this);
+  }
+  onChangeStatus(e) {
+    const status = e.target.value;
+    const { history } = this.props;
+    history.push({
+      pathname: "/issues",
+      search: status ? `?status=${status}` : "",
+    });
+  }
   render() {
     return (
       <div>
-        <Link to="/issues">All Issues</Link>
+        {/* <Link to="/issues">All Issues</Link>
         {" | "}
         <Link to={{ pathname: "/issues", search: "?status=New" }}>
           New Issues
@@ -12,8 +24,20 @@ export default class IssueFilter extends React.Component {
         {" | "}
         <Link to={{ pathname: "issues", search: "?status=Assigned" }}>
           Assigned Issues
-        </Link>
+        </Link> */}
+        <div>
+          Status:{" "}
+          <select onChange={this.onChangeStatus}>
+            <option value="">(All)</option>
+            <option value="New">New Issues</option>
+            <option value="Assigned">Assigned Issues</option>
+            <option value="Fixed">Fixed</option>
+            <option value="Closed">Closed</option>
+          </select>
+        </div>
       </div>
     );
   }
 }
+
+export default withRouter(IssueFilter);
