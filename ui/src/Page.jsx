f558ed9d6cf898c28760874ee64d8f23c0c2c370
Abhiroopmokshagna <abhiroopmokshagnabheemineni@gmail.com>
2022-04-26 17:38:46 +0530

    Inline Forms

--- ui/src/Page.jsx
+++ ui/src/Page.jsx
@@ -54,6 +54,7 @@ function NavBar() {
 function Footer() {
   return (
     <small>
+      <hr />
       <p className="text-center">
         Full source code available at this{" "}
         <a href="https://github.com/Abhiroopmokshagna/PRO-MERN-Stack">
