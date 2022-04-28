29d2202742372f7407c36f087d2c87b6f2f14ce0
Abhiroopmokshagna <abhiroopmokshagnabheemineni@gmail.com>
2022-04-28 13:03:56 +0530

    server router

--- ui/server/render.jsx
+++ ui/server/render.jsx
@@ -1,11 +1,17 @@
 const React = require("react");
 const ReactDOMServer = require("react-dom/server");
 
-import About from "../src/About.jsx";
+import { StaticRouter } from "react-router-dom";
+import Page from "../src/Page.jsx";
 import template from "./template.js";
 
 function render(req, res) {
-  const body = ReactDOMServer.renderToString(<About />);
+  const element = (
+    <StaticRouter location={req.url} context={{}}>
+      <Page />
+    </StaticRouter>
+  );
+  const body = ReactDOMServer.renderToString(element);
   res.send(template(body));
 }
 
